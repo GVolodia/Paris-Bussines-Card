e3d2cd70d2f7c633fb84a576e9794cceaa7e118c
Aficionado <volodiag@mail.ru>
2021-11-07 15:27:41 +0200

    Initial Commit

---
+++ BussinesCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BussinesCard
+//
+//  Created by noone on 07.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
